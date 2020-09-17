16dfdcc54f3a6caee907d1c8f01f994024bbf947
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-17 13:17:15 +0700

    FLUTTER 05. Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Row dan Column"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Text 1"),
-            Text("Text 2"),
-            Text("Text 3"),
-            Row(
-              children: <Widget>[
-                Text("Text 4"),
-                Text("Text 5"),
-                Text("Text 6"),
-              ],
-            )
-          ],
+        appBar: AppBar(title: Text("Latihan Container")),
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
+          padding: EdgeInsets.only(bottom: 20, top: 20),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                      Colors.amber,
+                      Colors.blue,
+                    ])),
+          ),
         ),
       ),
     );
