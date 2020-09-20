a128672ba9edefda6bd9585e18fdc1965e3005f4
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-20 11:28:28 +0700

    FLUTTER 22. InkWell (Membuat Sendiri Button Gradasi)

--- lib/main.dart
+++ lib/main.dart
@@ -6,43 +6,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan Membuat Button")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              RaisedButton(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                color: Colors.amber,
+                child: Text("Raised Button"),
+                shape: StadiumBorder(),
+                onPressed: () {},
+                splashColor: Colors.white60,
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(20),
+                elevation: 2,
+                child: Container(
+                  width: 150,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Colors.purple, Colors.pink],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      splashColor: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Center(
+                          child: Text(
+                        "My Button",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("MediaQuery")),
-      body: (MediaQuery.of(context).orientation == Orientation.portrait)
-          ? Column(
-              children: makeContainers(),
-            )
-          : Row(
-              children: makeContainers(),
-            ),
-    );
-  }
-
-  List<Widget> makeContainers() {
-    return <Widget>[
-      Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.amber,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.blue,
-        width: 100,
-        height: 100,
-      ),
-    ];
   }
 }
