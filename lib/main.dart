291f8630a9cd0a69bd0c3a4c41afddf696cd07d5
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-20 10:33:39 +0700

    FLUTTER 19. TextField Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,106 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
+        appBar: AppBar(title: Text("TextField Widget")),
         body: Container(
-          margin: EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              BuildCard(Icons.account_box, "Account Box"),
-              BuildCard(Icons.adb, "Serangga Android"),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class BuildCard extends StatelessWidget {
-  // const BuildCard({
-  //   Key key,
-  //   IconData iconData,
-  //   String text
-  // }) : super(key: key);
-
-  final IconData iconData;
-  final String text;
-
-  BuildCard(this.iconData, this.text);
-
-  @override
-  Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 0.7,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Card(
-          elevation: 5,
-          child: Row(
+          margin: EdgeInsets.all(20),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Flexible(
-                flex: 1,
-                child: Container(
-                  margin: EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(5),
-                        child: Icon(
-                          this.iconData,
-                          color: Colors.green,
-                          size: 50,
-                        ),
-                      ),
-                      Text(this.text),
-                    ],
-                  ),
-                ),
+              TextField(
+                maxLength: 5,
+                maxLines: 1,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: controller,
               ),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  margin: EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(5),
-                        child: Icon(
-                          this.iconData,
-                          color: Colors.green,
-                          size: 50,
-                        ),
-                      ),
-                      Text(this.text),
-                    ],
-                  ),
-                ),
-              ),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  margin: EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(5),
-                        child: Icon(
-                          this.iconData,
-                          color: Colors.green,
-                          size: 50,
-                        ),
-                      ),
-                      Text(this.text),
-                    ],
-                  ),
-                ),
-              ),
+              Text(controller.text),
             ],
           ),
         ),
