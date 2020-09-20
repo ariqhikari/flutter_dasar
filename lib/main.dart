08087d902ccd1e08acad81bae7f80ee5c355cc49
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-20 10:46:54 +0700

    FLUTTER 20. TextField (Decoration)

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,21 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  icon: Icon(Icons.adb),
+                  prefixIcon: Icon(Icons.person),
+                  suffixText: "Name : ",
+                  suffixStyle: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.bold),
+                  labelText: "Nama Lengkap",
+                  hintText: "Nama lengkapnya lho..",
+                  hintStyle: TextStyle(fontSize: 12),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
                 maxLength: 5,
-                maxLines: 1,
                 onChanged: (value) {
                   setState(() {});
                 },
