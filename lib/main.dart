baa2cd0aa5c08fc9527da6c8c7acd6c047593571
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-21 11:11:43 +0700

    FLUTTER 25. Hero & ClipRRect Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,220 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: MainPage(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  double getSmallDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 2 / 3;
-
-  double getBigDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 7 / 8;
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFEEEEEE),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            right: -getSmallDiameter(context) / 3,
-            top: -getSmallDiameter(context) / 3,
+      backgroundColor: Colors.lightBlue[50],
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Latihan Hero Animation",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return SecondPage();
+          }));
+        },
+        child: Hero(
+          tag: "pp",
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
             child: Container(
-              width: getSmallDiameter(context),
-              height: getSmallDiameter(context),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
+              width: 100,
+              height: 100,
+              child: Image(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    "https://img.beritasatu.com/cache/beritasatu/910x580-2/1542169847.jpg"),
               ),
             ),
           ),
-          Positioned(
-            left: -getBigDiameter(context) / 4,
-            top: -getBigDiameter(context) / 4,
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[50],
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Latihan Hero Animation",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Hero(
+          tag: "pp",
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
             child: Container(
-              child: Center(
-                child: Text(
-                  "dribbble",
-                  style: TextStyle(
-                      fontFamily: "Pacifico",
-                      fontSize: 30,
-                      color: Colors.white),
-                ),
-              ),
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
+              width: 200,
+              height: 200,
+              child: Image(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    "https://img.beritasatu.com/cache/beritasatu/910x580-2/1542169847.jpg"),
               ),
             ),
           ),
-          Positioned(
-            right: -getBigDiameter(context) / 2,
-            bottom: -getBigDiameter(context) / 2,
-            child: Container(
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color(0xFFF3E9EE),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ListView(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.email, color: Color(0xFFFF4891)),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color(0xFFFF4891),
-                            ),
-                          ),
-                          labelText: "Email: ",
-                          labelStyle: TextStyle(color: Color(0xFFFF4891)),
-                        ),
-                      ),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.vpn_key, color: Color(0xFFFF4891)),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color(0xFFFF4891),
-                            ),
-                          ),
-                          labelText: "Password: ",
-                          labelStyle: TextStyle(color: Color(0xFFFF4891)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
-                    child: Text(
-                      "FORGOT PASSWORD?",
-                      style: TextStyle(
-                        color: Color(0xFFFF4891),
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 0, 20, 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        height: 40,
-                        child: Container(
-                          child: Material(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.transparent,
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(20),
-                              splashColor: Colors.amber,
-                              onTap: () {},
-                              child: Center(
-                                child: Text(
-                                  "SIGN IN",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            gradient: LinearGradient(
-                              colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                            ),
-                          ),
-                        ),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () {},
-                        mini: true,
-                        elevation: 0,
-                        backgroundColor: Colors.transparent,
-                        child: SvgPicture.asset("assets/icons/facebook.svg"),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () {},
-                        mini: true,
-                        elevation: 0,
-                        backgroundColor: Colors.transparent,
-                        child: SvgPicture.asset("assets/icons/facebook.svg"),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "DON'T HAVE AN ACCOUNT?  ",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Text(
-                      "SIGN UP",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w700,
-                        color: Color(0xFFFF4891),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
