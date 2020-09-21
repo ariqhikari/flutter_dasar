eb114ddb65beaf4b3084a6f8d10bf220e29faa59
ariqhikari <59524975+ariqhikari@users.noreply.github.com>
2020-09-21 16:31:01 +0700

    FLUTTER 27. TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(200),
-          child: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.amber,
-            flexibleSpace: Positioned(
-              bottom: 0,
-              right: 0,
-              child: Container(
-                margin: EdgeInsets.all(20),
-                child: Text(
-                  "Appbar with Custom Height",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Contoh Tab Bar"),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.comment),
+                  text: "Comments",
+                ),
+                Tab(
+                  child: Image(
+                    image: NetworkImage(
+                        "https://img2.pngio.com/food-computer-icons-peach-cute-icons-free-png-pngfuel-cute-icons-png-910_512.png"),
                   ),
                 ),
-              ),
+                Tab(icon: Icon(Icons.computer)),
+                Tab(
+                  text: "News",
+                ),
+              ],
             ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Center(
+                child: Text("Tab 1"),
+              ),
+              Center(
+                child: Text("Tab 2"),
+              ),
+              Center(
+                child: Text("Tab 3"),
+              ),
+              Center(
+                child: Text("Tab 4"),
+              ),
+            ],
           ),
         ),
       ),
