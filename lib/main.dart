2f083e47c02ddb1b12e54c7bf9d389e647d49145
ariqhikari <59524975+ariqhikari@users.noreply.github.com>
2020-09-21 17:09:41 +0700

    FLUTTER 29. Menampilkan QR Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,22 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  TabBar myTabBar = TabBar(
-    indicator: BoxDecoration(
-      color: Colors.red,
-      border: Border(
-        top: BorderSide(
-          color: Colors.purple,
-          width: 5,
-        ),
-      ),
-    ),
-    tabs: <Widget>[
-      Tab(
-        icon: Icon(Icons.comment),
-        text: "Comments",
-      ),
-      Tab(
-        icon: Icon(Icons.computer),
-        text: "Computers",
-      ),
-    ],
-  );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Contoh Tab Bar"),
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
-              child: Container(color: Colors.amber, child: myTabBar),
-            ),
-          ),
-          body: TabBarView(
-            children: <Widget>[
-              Center(
-                child: Text("Tab 1"),
-              ),
-              Center(
-                child: Text("Tab 2"),
-              ),
-            ],
+      home: Scaffold(
+        body: Center(
+          child: QrImage(
+            version: 6,
+            backgroundColor: Colors.grey,
+            foregroundColor: Colors.black,
+            errorCorrectionLevel: QrErrorCorrectLevel.M,
+            padding: EdgeInsets.all(30),
+            size: 300,
+            data: "http://hikari.miraistudio.id/",
           ),
         ),
       ),
