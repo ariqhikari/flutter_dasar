6d072f6fecf2a723a27d3e5fa6cd3be167f13d51
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-22 16:26:20 +0700

    FLUTTER 32. Memainkan Musik / Sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  AudioPlayer audioPlayer;
+  String duration = "00:00:00";
+
+  _MyAppState() {
+    audioPlayer = AudioPlayer();
+    audioPlayer.onAudioPositionChanged.listen((event) {
+      setState(() {
+        print("Odading " + event.toString());
+        duration = event.toString();
+      });
+      audioPlayer.setReleaseMode(ReleaseMode.LOOP);
+    });
+  }
+
+  void playSound(String url) async {
+    await audioPlayer.seek(Duration(seconds: 30));
+    await audioPlayer.play(url);
+  }
+
+  void pauseSound() async {
+    await audioPlayer.pause();
+  }
+
+  void stopSound() async {
+    await audioPlayer.stop();
+  }
+
+  void resumeSound() async {
+    await audioPlayer.resume();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Gradient Opacity")),
+        appBar: AppBar(title: Text("Playing Music")),
         body: Center(
-          child: ShaderMask(
-            shaderCallback: (rectangle) {
-              return LinearGradient(
-                colors: [Colors.black, Colors.transparent],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ).createShader(
-                  Rect.fromLTRB(0, 0, rectangle.width, rectangle.height));
-            },
-            blendMode: BlendMode.dstIn,
-            child: Image(
-              width: 700,
-              image: NetworkImage(
-                  "https://images.unsplash.com/photo-1525316166484-aeef34af3ec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"),
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Play"),
+                onPressed: () {
+                  playSound(
+                      "http://dl17.freemp3downloads.online/file/youtubedGcGbF4ex5o128.mp3?fn=Sheila%20on%207%20-%20Dan.mp3");
+                },
+              ),
+              RaisedButton(
+                child: Text("Pause"),
+                onPressed: () {
+                  pauseSound();
+                },
+              ),
+              RaisedButton(
+                child: Text("Stop"),
+                onPressed: () {
+                  stopSound();
+                },
+              ),
+              RaisedButton(
+                child: Text("Resume"),
+                onPressed: () {
+                  resumeSound();
+                },
+              ),
+              Text(
+                duration,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
           ),
         ),
       ),
