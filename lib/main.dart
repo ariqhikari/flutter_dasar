5b3e09c7dd959dec5dfabab24875ebf7e8c7f2db
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-29 14:47:24 +0700

    FLUTTER 35. ClipPath

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,71 +6,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Font Features',
-      theme: ThemeData(fontFamily: 'Montserrat'),
       home: Scaffold(
-        appBar: AppBar(title: Text("Font Features")),
+        appBar: AppBar(title: Text("ClipPath")),
         body: Center(
-          child: ListView(children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  "Contoh 01 (Tanpa apapun)",
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                Text(
-                  "Contoh 02 (Small Caps)",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFeatures: [FontFeature.enable("smcp")],
-                  ),
-                ),
-                Text(
-                  "Contoh 1/2 (Small Caps & Frac)",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFeatures: [
-                      FontFeature.enable("smcp"),
-                      FontFeature.enable("frac"),
-                    ],
-                  ),
-                ),
-                Text(
-                  "Contoh Montserrat 19 (Tanpa apapun)",
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                Text(
-                  "Contoh Montserrat 19 (Old Style)",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFeatures: [FontFeature.oldstyleFigures()],
-                  ),
-                ),
-                Text(
-                  "Contoh Pacifico (Default)",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: "Pacifico",
-                  ),
-                ),
-                Text(
-                  "Contoh Pacifico (Style set nomor 5)",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: "Pacifico",
-                    fontFeatures: [FontFeature.stylisticSet(5)],
-                  ),
-                ),
-              ],
+          child: ClipPath(
+            clipper: MyClipper(),
+            child: Image(
+              image: NetworkImage(
+                  "https://gamestation.co.id/wp-content/uploads/2018/09/New-Super-Mario-01-735x400.jpg"),
             ),
-          ]),
+          ),
         ),
       ),
     );
   }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2, size.height * 0.75, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
