16b9db64cd3312572d7543d7725e71c293e8d84c
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-30 16:44:15 +0700

    FLUTTER 40. AnimatedPadding Widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,81 +8,72 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool isON = false;
-  Widget myWidget = Container(
-    width: 200,
-    height: 100,
-    decoration: BoxDecoration(
-      color: Colors.red,
-      border: Border.all(color: Colors.black, width: 3),
-    ),
-  );
+  double myPadding = 5;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Switch & AnimatedSwitcher Widget")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              AnimatedSwitcher(
-                child: myWidget,
-                duration: Duration(seconds: 1),
-                transitionBuilder: (child, animation) => ScaleTransition(
-                  scale: animation,
-                  child: child,
-                ),
-              ),
-              Switch(
-                activeColor: Colors.green,
-                inactiveThumbColor: Colors.red,
-                inactiveTrackColor: Colors.red[200],
-                value: isON,
-                onChanged: (newValue) {
-                  isON = newValue;
-                  setState(() {
-                    if (isON) {
-                      myWidget = Container(
-                        key: ValueKey(1),
-                        width: 200,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          border: Border.all(color: Colors.black, width: 3),
+        appBar: AppBar(title: Text("AnimatedPadding")),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+                flex: 1,
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                      flex: 1,
+                      child: AnimatedPadding(
+                        duration: Duration(seconds: 1),
+                        padding: EdgeInsets.all(myPadding),
+                        child: GestureDetector(
+                          child: Container(color: Colors.red),
+                          onTap: () {
+                            setState(() {
+                              myPadding = 20;
+                            });
+                          },
+                          onTapCancel: () {
+                            setState(() {
+                              myPadding = 5;
+                            });
+                          },
                         ),
-                      );
-                      // myWidget = SizedBox(
-                      //   width: 200,
-                      //   height: 100,
-                      //   child: Center(
-                      //     child: Text(
-                      //       "Swith : ON",
-                      //       style: TextStyle(
-                      //         color: Colors.green,
-                      //         fontWeight: FontWeight.bold,
-                      //         fontSize: 20,
-                      //       ),
-                      //     ),
-                      //   ),
-                      // );
-                    } else {
-                      myWidget = Container(
-                        key: ValueKey(2),
-                        width: 200,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          border: Border.all(color: Colors.black, width: 3),
-                        ),
-                      );
-                    }
-                  });
-                },
-              ),
-            ],
-          ),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: AnimatedPadding(
+                        duration: Duration(seconds: 1),
+                        padding: EdgeInsets.all(myPadding),
+                        child: Container(color: Colors.green),
+                      ),
+                    ),
+                  ],
+                )),
+            Flexible(
+                flex: 1,
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                      flex: 1,
+                      child: AnimatedPadding(
+                        duration: Duration(seconds: 1),
+                        padding: EdgeInsets.all(myPadding),
+                        child: Container(color: Colors.blue),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: AnimatedPadding(
+                        duration: Duration(seconds: 1),
+                        padding: EdgeInsets.all(myPadding),
+                        child: Container(color: Colors.yellow),
+                      ),
+                    ),
+                  ],
+                )),
+          ],
         ),
       ),
     );
