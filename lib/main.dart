1d417018cd29228e14a2a459d46ce1a09ad80d31
Ariq Hikari <ahidayatbia@gmail.com>
2020-09-30 08:46:38 +0700

    FLUTTER 39. Switch & AnimatedSwitcher Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_flutter/api/user_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,27 +8,76 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String output = "No data";
+  bool isON = false;
+  Widget myWidget = Container(
+    width: 200,
+    height: 100,
+    decoration: BoxDecoration(
+      color: Colors.red,
+      border: Border.all(color: Colors.black, width: 3),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("HTTP Request")),
+        appBar: AppBar(title: Text("Switch & AnimatedSwitcher Widget")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Text(output),
-              RaisedButton(
-                child: Text("GET"),
-                onPressed: () {
-                  User.getUsers().then((users) {
-                    output = "";
-                    for (int i = 0; i < users.length; i++) {
-                      output = output + "[ " + users[i].name + " ] ";
+              AnimatedSwitcher(
+                child: myWidget,
+                duration: Duration(seconds: 1),
+                transitionBuilder: (child, animation) => ScaleTransition(
+                  scale: animation,
+                  child: child,
+                ),
+              ),
+              Switch(
+                activeColor: Colors.green,
+                inactiveThumbColor: Colors.red,
+                inactiveTrackColor: Colors.red[200],
+                value: isON,
+                onChanged: (newValue) {
+                  isON = newValue;
+                  setState(() {
+                    if (isON) {
+                      myWidget = Container(
+                        key: ValueKey(1),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          border: Border.all(color: Colors.black, width: 3),
+                        ),
+                      );
+                      // myWidget = SizedBox(
+                      //   width: 200,
+                      //   height: 100,
+                      //   child: Center(
+                      //     child: Text(
+                      //       "Swith : ON",
+                      //       style: TextStyle(
+                      //         color: Colors.green,
+                      //         fontWeight: FontWeight.bold,
+                      //         fontSize: 20,
+                      //       ),
+                      //     ),
+                      //   ),
+                      // );
+                    } else {
+                      myWidget = Container(
+                        key: ValueKey(2),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          border: Border.all(color: Colors.black, width: 3),
+                        ),
+                      );
                     }
-                    setState(() {});
                   });
                 },
               ),
