5c8a88355202ce009cdc2968e509e1dc21c917af
Ariq Hikari <ahidayatbia@gmail.com>
2020-10-04 11:43:40 +0700

    FLUTTER 52. ListView Builder + BLoC

--- lib/bloc/color_bloc.dart
+++ lib/bloc/color_bloc.dart
@@ -5,15 +5,8 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 enum ColorEvent { to_amber, to_lightBlue }
 
 /// Class ini digunakan untuk merubah state warna pada tampilan
-<<<<<<< HEAD
 class ColorBloc extends HydratedBloc<ColorEvent, Color> {
   /// [initialState] diisi dengan **warna** yang ingin dijadikan sebagai **warna** default
-=======
-class ColorBloc extends Bloc<ColorEvent, Color> {
-  Color _color = Colors.amber;
-
-  /// [initialState] diisi dengan **warna** yang ingin dijadikan sebagai `warna` default
->>>>>>> a383649128d9653f8c457572b0159e1269d8e200
   ///
   /// Contoh:
   /// ```
--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,106 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_first_flutter/bloc/color_bloc.dart';
-import 'package:bloc/bloc.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'dart:math';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  HydratedBloc.storage = await HydratedStorage.build();
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'includes/product_card.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: firstColor,
-          title: Text("Product Card"),
-        ),
-        body: ChangeNotifierProvider<ProductState>(
-          create: (context) => ProductState(),
-          child: Container(
-            margin: EdgeInsets.all(20),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Consumer<ProductState>(
-                builder: (context, productState, _) => ProductCard(
-                  imageURL:
-                      "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg",
-                  name: "Buah-buahan Mix 1 kg",
-                  price: "Rp25.000",
-                  quantity: productState.quantity,
-                  notification:
-                      (productState.quantity > 5) ? "Diskon 10%" : null,
-                  onAddCartTap: () {},
-                  onIncTap: () {
-                    productState.quantity++;
-                  },
-                  onDecTap: () {
-                    productState.quantity--;
-                  },
-                ),
-              ),
-            ),
-          ),
-        ),
+      home: BlocProvider(
+        create: (context) => ProductBloc([]),
+        child: MainPage(),
       ),
     );
   }
 }
 
-class ProductState with ChangeNotifier {
-  int _quantity = 0;
+class MainPage extends StatelessWidget {
+  final Random r = Random();
 
-  int get quantity => _quantity;
-  set quantity(int newValue) {
-    _quantity = newValue;
-    notifyListeners();
+  @override
+  Widget build(BuildContext context) {
+    ProductBloc bloc = BlocProvider.of<ProductBloc>(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0XFFF44336),
+        title: Text("Demo ListView Builder"),
+      ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            color: Color(0XFFF44336),
+            child: Text(
+              "Create Products",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {
+              bloc.add(r.nextInt(4) + 2);
+            },
+          ),
+          SizedBox(height: 20),
+          BlocBuilder<ProductBloc, List<Product>>(
+            builder: (context, products) => Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      (index == 0) ? SizedBox(width: 20) : Container(),
+                      ProductCard(
+                        imageURL: products[index].imageURL,
+                        name: products[index].name,
+                        price: products[index].price.toString(),
+                        onAddCartTap: () {},
+                        onIncTap: () {},
+                        onDecTap: () {},
+                      ),
+                      SizedBox(width: 20),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Product {
+  String imageURL;
+  String name;
+  int price;
+
+  Product({this.imageURL = "", this.name = "", this.price = 0});
+}
+
+class ProductBloc extends Bloc<int, List<Product>> {
+  ProductBloc(List<Product> initialState) : super(initialState);
+
+  @override
+  Stream<List<Product>> mapEventToState(int event) async* {
+    List<Product> products = [];
+    for (int i = 0; i < event; i++) {
+      products.add(
+        Product(
+          imageURL:
+              "https://hikari.miraistudio.id/frontend/assets/images/about.jpg",
+          name: "Jual Temen " + (i + 1).toString(),
+          price: (i + 1) * 5000,
+        ),
+      );
+    }
+    yield products;
   }
 }
