5b697517f9292c8d5d04f12be6ae2f1ccf7ffdbb
Ariq Hikari <ahidayatbia@gmail.com>
2020-10-06 08:26:02 +0700

    FLUTTER 50. Flutter 1.9 (SelectableText, ToggleButtons, ColorFiltered)

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,71 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'includes/product_card.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<bool> isSelected = [true, false, false];
+  ColorFilter colorFilter = ColorFilter.mode(Colors.blue, BlendMode.screen);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) => ProductBloc([]),
-        child: MainPage(),
-      ),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  final Random r = Random();
-
-  @override
-  Widget build(BuildContext context) {
-    ProductBloc bloc = BlocProvider.of<ProductBloc>(context);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0XFFF44336),
-        title: Text("Demo ListView Builder"),
-      ),
-      body: Column(
-        children: <Widget>[
-          RaisedButton(
-            color: Color(0XFFF44336),
-            child: Text(
-              "Create Products",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            onPressed: () {
-              bloc.add(r.nextInt(4) + 2);
-            },
-          ),
-          SizedBox(height: 20),
-          BlocBuilder<ProductBloc, List<Product>>(
-            builder: (context, products) => Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  return Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      (index == 0) ? SizedBox(width: 20) : Container(),
-                      ProductCard(
-                        imageURL: products[index].imageURL,
-                        name: products[index].name,
-                        price: products[index].price.toString(),
-                        onAddCartTap: () {},
-                        onIncTap: () {},
-                        onDecTap: () {},
-                      ),
-                      SizedBox(width: 20),
-                    ],
-                  );
-                },
-              ),
+      home: ColorFiltered(
+        colorFilter: colorFilter,
+        child: Scaffold(
+          appBar: AppBar(title: Text("Flutter 1.9")),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SelectableText(
+                  "Ini adalah Selectable Text. Silahkan pilih saya.",
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                  showCursor: true,
+                  cursorWidth: 3,
+                  cursorColor: Colors.red,
+                ),
+                SizedBox(height: 20),
+                ToggleButtons(
+                  isSelected: isSelected,
+                  children: <Widget>[
+                    Icon(Icons.access_alarm),
+                    Icon(Icons.adb),
+                    Icon(Icons.add_comment),
+                  ],
+                  onPressed: (index) {
+                    setState(() {
+                      if (index == 0) {
+                        colorFilter =
+                            ColorFilter.mode(Colors.blue, BlendMode.screen);
+                      } else if (index == 1) {
+                        colorFilter =
+                            ColorFilter.mode(Colors.green, BlendMode.softLight);
+                      } else {
+                        colorFilter =
+                            ColorFilter.mode(Colors.purple, BlendMode.darken);
+                      }
+                      for (int i = 0; i < isSelected.length; i++) {
+                        isSelected[i] = (i == index) ? true : false;
+                      }
+                    });
+                  },
+                  fillColor: Colors.red[50],
+                  selectedColor: Colors.red,
+                  selectedBorderColor: Colors.red,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class Product {
-  String imageURL;
-  String name;
-  int price;
-
-  Product({this.imageURL = "", this.name = "", this.price = 0});
-}
-
-class ProductBloc extends Bloc<int, List<Product>> {
-  ProductBloc(List<Product> initialState) : super(initialState);
-
-  @override
-  Stream<List<Product>> mapEventToState(int event) async* {
-    List<Product> products = [];
-    for (int i = 0; i < event; i++) {
-      products.add(
-        Product(
-          imageURL:
-              "https://hikari.miraistudio.id/frontend/assets/images/about.jpg",
-          name: "Jual Temen " + (i + 1).toString(),
-          price: (i + 1) * 5000,
         ),
-      );
-    }
-    yield products;
+      ),
+    );
   }
 }
