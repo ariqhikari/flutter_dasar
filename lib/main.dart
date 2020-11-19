90f19d39721f9ef09772b19d7091d2c53add6a6c
Ariq Hikari <ahidayatbia@gmail.com>
2020-11-19 20:11:21 +0700

    FLUTTER 57. Hive - Database in Pure Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter/model/monster.dart';
+import 'package:my_first_flutter/ui/main_page.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
+import 'package:hive/hive.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var appDocumentDirectory =
+      await pathProvider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  Hive.registerAdapter(MonsterAdapter());
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,71 +21,4 @@ class MyApp extends StatelessWidget {
       home: MainPage(),
     );
   }
-}
-
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  Person selectedPerson;
-  List<Person> persons = [
-    Person("Joni"),
-    Person("Joko"),
-  ];
-
-  List<DropdownMenuItem> generateItems(List<Person> persons) {
-    List<DropdownMenuItem> items = [];
-    for (var person in persons) {
-      items.add(
-        DropdownMenuItem(
-          child: Text(person.name),
-          value: person,
-        ),
-      );
-    }
-    return items;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Demo Dropdown Button"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(20),
-            child: DropdownButton(
-              isExpanded: true,
-              style: TextStyle(fontSize: 20, color: Colors.purple),
-              value: selectedPerson,
-              items: generateItems(persons),
-              onChanged: (item) {
-                setState(() {
-                  selectedPerson = item;
-                });
-              },
-            ),
-          ),
-          Text(
-            (selectedPerson != null)
-                ? selectedPerson.name
-                : "Belum ada yang terpilih",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Person {
-  String name;
-  Person(this.name);
 }
