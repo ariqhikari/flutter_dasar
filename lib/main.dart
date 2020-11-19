14ce614484fffdbaa02661c66a3eb01afe829cbd
Ariq Hikari <ahidayatbia@gmail.com>
2020-11-19 18:45:29 +0700

    FLUTTER 56. Drop Down Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_first_flutter/bloc/color_bloc.dart';
-import 'package:my_first_flutter/bloc/counter_bloc.dart';
-import 'package:my_first_flutter/ui/main_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ColorBloc>(
-          create: (context) => ColorBloc(Colors.pink),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  Person selectedPerson;
+  List<Person> persons = [
+    Person("Joni"),
+    Person("Joko"),
+  ];
+
+  List<DropdownMenuItem> generateItems(List<Person> persons) {
+    List<DropdownMenuItem> items = [];
+    for (var person in persons) {
+      items.add(
+        DropdownMenuItem(
+          child: Text(person.name),
+          value: person,
         ),
-        BlocProvider<CounterBloc>(
-          create: (context) => CounterBloc(0),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MainPage(),
+      );
+    }
+    return items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Dropdown Button"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(20),
+            child: DropdownButton(
+              isExpanded: true,
+              style: TextStyle(fontSize: 20, color: Colors.purple),
+              value: selectedPerson,
+              items: generateItems(persons),
+              onChanged: (item) {
+                setState(() {
+                  selectedPerson = item;
+                });
+              },
+            ),
+          ),
+          Text(
+            (selectedPerson != null)
+                ? selectedPerson.name
+                : "Belum ada yang terpilih",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class Person {
+  String name;
+  Person(this.name);
 }
