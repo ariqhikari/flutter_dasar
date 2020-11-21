57841ee3fbf764e617fa8e655462b727ac46f261
Ariq Hikari <ahidayatbia@gmail.com>
2020-11-21 17:50:04 +0700

    FLUTTER 59. Flutter_bloc ver 1.0 & Bloc Visual Studio Code Plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_first_flutter/bloc/post_bloc.dart';
-import 'package:my_first_flutter/ui/main_page.dart';
+import 'package:my_first_flutter/bloc/counter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,9 +9,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BlocProvider<PostBloc>(
-          create: (context) => PostBloc(PostUninitialized())..add(PostEvent()),
-          child: MainPage()),
+      home: BlocProvider<CounterBloc>(
+        create: (context) => CounterBloc(),
+        child: MainPage(),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CounterBloc cBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Bloc Ver 1.0"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BlocBuilder<CounterBloc, CounterState>(
+              builder: (context, counterState) => Text(
+                counterState.value.toString(),
+                style: TextStyle(
+                  fontSize: 80,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  child: Icon(Icons.arrow_downward),
+                  onPressed: () {
+                    cBloc.add(Decrement());
+                  },
+                ),
+                SizedBox(width: 20),
+                FloatingActionButton(
+                  child: Icon(Icons.arrow_upward),
+                  onPressed: () {
+                    cBloc.add(Increment());
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
