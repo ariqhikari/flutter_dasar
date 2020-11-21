204d2929d6316351f65b8f0346dc1a82c069ee64
Ariq Hikari <ahidayatbia@gmail.com>
2020-11-21 18:56:33 +0700

    FLUTTER 60. Extension

--- lib/bloc/counter_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'counter_bloc.dart';
-
-@immutable
-class CounterState {
-  final int value;
-
-  CounterState(this.value);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_first_flutter/bloc/counter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,58 +7,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BlocProvider<CounterBloc>(
-        create: (context) => CounterBloc(),
-        child: MainPage(),
-      ),
+      home: MainPage(),
     );
   }
 }
 
 class MainPage extends StatelessWidget {
+  final int x = -10;
+
   @override
   Widget build(BuildContext context) {
-    CounterBloc cBloc = BlocProvider.of<CounterBloc>(context);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Bloc Ver 1.0"),
-      ),
+      appBar: AppBar(title: Text("Extension Demo")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<CounterBloc, CounterState>(
-              builder: (context, counterState) => Text(
-                counterState.value.toString(),
-                style: TextStyle(
-                  fontSize: 80,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FloatingActionButton(
-                  child: Icon(Icons.arrow_downward),
-                  onPressed: () {
-                    cBloc.add(Decrement());
-                  },
-                ),
-                SizedBox(width: 20),
-                FloatingActionButton(
-                  child: Icon(Icons.arrow_upward),
-                  onPressed: () {
-                    cBloc.add(Increment());
-                  },
-                ),
-              ],
-            ),
-          ],
+        child: Text(
+          "Bilangan:" & x.negate().toString(),
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
   }
+}
+
+extension NumberExtension<T extends num> on num {
+  T negate() => -1 * this;
+}
+
+extension StringExtension on String {
+  String operator &(String other) => this + " " + other;
 }
