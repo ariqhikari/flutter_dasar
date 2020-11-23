b415d830ffe08fbbde670d32d2c1d18e44d010a4
Ariq Hikari <ahidayatbia@gmail.com>
2020-11-23 09:45:45 +0700

    FLUTTER 66. Accessibility - Semantics Widget (Flutter Interact '19)

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,16 +1,50 @@
+import 'package:division/division.dart';
 import 'package:flutter/material.dart';
-import 'package:my_first_flutter/ui/switch_day_night.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int number = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text("Rive + Flutter"),
+          title: Semantics(
+        hidden: true,
+        excludeSemantics: true,
+        child: Text("Aplikasi Aksesibilitas"),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Semantics(
+          onTapHint: "menambahkan bilangan dengan satu.",
+          child: Icon(Icons.plus_one),
+        ),
+        onPressed: () {
+          setState(() {
+            number++;
+          });
+        },
       ),
       body: Center(
-        child: SwitchDayNight(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Semantics(
+              label:
+                  "Ini adalah bilangan yang akan ditambahkan. Bilangan saat ini adalah",
+              child: Txt(
+                number.toString(),
+                style: TxtStyle()
+                  ..fontSize(60)
+                  ..bold(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
