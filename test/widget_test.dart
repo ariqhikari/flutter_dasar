773fd4efa6e2ae673a1271e8f7ff875436cdc0ca
Ariq Hikari <ahidayatbia@gmail.com>
2020-11-26 10:21:37 +0700

    FLUTTER 69. Equatable

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:my_first_flutter/main.dart';
+import 'package:my_first_flutter/person.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  Person p = Person(name: "Dodi", age: 10);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  if (p == Person(name: "Dodi", age: 10)) {
+    print("sama");
+  } else {
+    print("tidak sama");
+  }
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  List<Person> persons = [p];
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  if (persons.contains(Person(name: "Dodi", age: 10))) {
+    print("ada");
+  } else {
+    print("tidak ada");
+  }
 }
