5487682b5d2e45a32ce3c1f85a0e5cf1aae18680
Ariq Hikari <ahidayatbia@gmail.com>
2020-11-27 14:43:34 +0700

    FLUTTER 73. Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.srirachaTextTheme(),
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Color fontColor = const Color(0XFFFFB401);
+    TextStyle textStyle = TextStyle(color: fontColor, fontSize: 32);
+
+    return Scaffold(
+      backgroundColor: const Color(0XFF1E252D),
+      appBar: AppBar(
+        backgroundColor: const Color(0XFF282F37),
+        title: Text("Google Fonts Demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "I'll keep learning Flutter!",
+              style: textStyle,
+            ),
+            Text(
+              "O yeah !!",
+              style: textStyle,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
