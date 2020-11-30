fa54e8de5ac435fe9b77cd915b14e7c6d227a1f3
Ariq Hikari <ahidayatbia@gmail.com>
2020-11-30 20:47:11 +0700

    FLUTTER 76. Cloud Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_first_flutter/services/auth_services.dart';
+import 'package:my_first_flutter/services/database_services.dart';
+import 'package:my_first_flutter/ui/error_page.dart';
+import 'package:my_first_flutter/ui/loading_page.dart';
 import 'package:my_first_flutter/wrapper.dart';
 import 'package:provider/provider.dart';
 
@@ -9,25 +13,89 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // return FutureBuilder(
+    //   future: Firebase.initializeApp(),
+    //   builder: (context, snapshot) {
+    //     if (snapshot.hasError) {
+    //       print(snapshot.error.toString());
+    //       return Text("Error.");
+    //     }
+
+    //     if (snapshot.connectionState == ConnectionState.done) {
+    //       return StreamProvider.value(
+    //         value: AuthServices.firebaseUserStream,
+    //         child: MaterialApp(
+    //           debugShowCheckedModeBanner: false,
+    //           home: Wrapper(),
+    //         ),
+    //       );
+    //     }
+
+    //     return Text("Loading, please wait.");
+    //   },
+    // );
     return FutureBuilder(
       future: Firebase.initializeApp(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           print(snapshot.error.toString());
-          return Text("Error.");
+          return ErrorPage();
         }
 
         if (snapshot.connectionState == ConnectionState.done) {
-          return StreamProvider.value(
-            value: AuthServices.firebaseUserStream,
-            child: MaterialApp(
-              debugShowCheckedModeBanner: false,
-              home: Wrapper(),
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(title: Text("Firsestore Demo")),
+              body: Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text("Add Data"),
+                        onPressed: () {
+                          DatabaseServices.createOrUpdateProduct(
+                            "1",
+                            name: "Masker",
+                            price: 1000000,
+                          );
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text("Edit Data"),
+                        onPressed: () {
+                          DatabaseServices.createOrUpdateProduct(
+                            "1",
+                            name: "Masker",
+                            price: 2000000,
+                          );
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text("Get Data"),
+                        onPressed: () async {
+                          DocumentSnapshot snapshot =
+                              await DatabaseServices.getProduct("1");
+                          print(snapshot.data()["nama"]);
+                          print(snapshot.data()["harga"]);
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text("Delete Data"),
+                        onPressed: () async {
+                          await DatabaseServices.deleteProduct("1");
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           );
         }
 
-        return Text("Loading, please wait.");
+        return LoadingPage();
       },
     );
   }
