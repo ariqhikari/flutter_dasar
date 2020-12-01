85e5a548609e98b72de39347b2b1ba851586433a
Ariq Hikari <ahidayatbia@gmail.com>
2020-12-01 17:51:51 +0700

    FLUTTER 78. Shimmer Effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,83 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_first_flutter/services/auth_services.dart';
-import 'package:my_first_flutter/services/database_services.dart';
-import 'package:my_first_flutter/ui/error_page.dart';
-import 'package:my_first_flutter/ui/loading_page.dart';
-import 'package:my_first_flutter/wrapper.dart';
-import 'package:provider/provider.dart';
+import 'package:shimmer/shimmer.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return FutureBuilder(
-    //   future: Firebase.initializeApp(),
-    //   builder: (context, snapshot) {
-    //     if (snapshot.hasError) {
-    //       print(snapshot.error.toString());
-    //       return Text("Error.");
-    //     }
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
 
-    //     if (snapshot.connectionState == ConnectionState.done) {
-    //       return StreamProvider.value(
-    //         value: AuthServices.firebaseUserStream,
-    //         child: MaterialApp(
-    //           debugShowCheckedModeBanner: false,
-    //           home: Wrapper(),
-    //         ),
-    //       );
-    //     }
-
-    //     return Text("Loading, please wait.");
-    //   },
-    // );
-    return FutureBuilder(
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          print(snapshot.error.toString());
-          return ErrorPage();
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: Scaffold(
-              appBar: AppBar(title: Text("Firsestore Demo")),
-              body: Container(
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      RaisedButton(
-                        child: Text("Add Data"),
-                        onPressed: () {
-                          DatabaseServices.createOrUpdateProduct(
-                            "1",
-                            name: "Masker",
-                            price: 1000000,
-                          );
-                        },
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Shimmer Demo")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: 200,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        "https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg",
                       ),
-                      RaisedButton(
-                        child: Text("Edit Data"),
-                        onPressed: () {
-                          DatabaseServices.createOrUpdateProduct(
-                            "1",
-                            name: "Masker",
-                            price: 2000000,
-                          );
-                        },
-                      ),
-                      RaisedButton(
-                        child: Text("Get Data"),
-                        onPressed: () async {
-                          DocumentSnapshot snapshot =
-                              await DatabaseServices.getProduct("1");
-                          print(snapshot.data()["nama"]);
-                          print(snapshot.data()["harga"]);
-                        },
-                      ),
-                      RaisedButton(
-                        child: Text("Delete Data"),
-                        onPressed: () async {
-                          await DatabaseServices.deleteProduct("1");
-                        },
-                      ),
-                    ],
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
+                Shimmer(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    stops: [0.4, 0.5, 0.6],
+                    colors: [
+                      Color.fromRGBO(224, 224, 224, 0),
+                      Color.fromRGBO(224, 224, 224, 0.8),
+                      Color.fromRGBO(224, 224, 224, 0),
+                    ],
+                  ),
+                  child: Container(
+                    width: 200,
+                    height: 300,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
             ),
-          );
-        }
-
-        return LoadingPage();
-      },
+            SizedBox(height: 20),
+            Shimmer(
+              direction: ShimmerDirection.rtl,
+              gradient: LinearGradient(
+                stops: [0.45, 0.5, 0.55],
+                colors: [
+                  Colors.black,
+                  Colors.white,
+                  Colors.black,
+                ],
+              ),
+              child: Text(
+                "Star Wars: The Rise of Skywalker",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
