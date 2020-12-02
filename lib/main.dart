4c27a36187e4b2902bc80776ff811d0ac40e1378
Ariq Hikari <ahidayatbia@gmail.com>
2020-12-02 16:47:07 +0700

    FLUTTER 80. Animasi Dasar

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  OneSignal.shared
-      .init("0204bb81-9b27-4d28-91a3-c8ffb25e4ffd", iOSSettings: null);
-  OneSignal.shared
-      .setInFocusDisplayType(OSNotificationDisplayType.notification);
-
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,54 +18,52 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
-  String title = "Title";
-  String content = "Content";
-  String url = "";
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
 
   @override
   void initState() {
     super.initState();
 
-    OneSignal.shared
-        .setNotificationReceivedHandler((OSNotification notification) {
-      setState(() {
-        title = notification.payload.title;
-        content = notification.payload.body;
-        url = notification.payload.bigPicture;
+    controller = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+    );
+    animation = Tween<double>(begin: 0, end: 2 * pi).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
       });
-    });
 
-    OneSignal.shared
-        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
-      print("Notifikasi di-tap");
-    });
+    controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("One Signal Demo")),
+      appBar: AppBar(title: Text("Basic Animation")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(title),
-            SizedBox(height: 20),
-            Text(content),
-            SizedBox(height: 20),
-            (url != "")
-                ? Container(
-                    width: 400,
-                    height: 500,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(url),
-                      ),
-                    ),
-                  )
-                : SizedBox(),
-          ],
+        child: Transform.rotate(
+          angle: animation.value,
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.red, Colors.purple],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
         ),
       ),
     );
