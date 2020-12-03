e486183563bde47a5354597b1d7d7880a2746983
Ariq Hikari <ahidayatbia@gmail.com>
2020-12-03 10:00:50 +0700

    FLUTTER 84. Biometric Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:local_auth/local_auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,39 +19,98 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-
-    if (state == AppLifecycleState.inactive) {
-      print("====> INACTIVE <====");
-    } else if (state == AppLifecycleState.paused) {
-      print("====> PAUSED <====");
-    } else if (state == AppLifecycleState.resumed) {
-      print("====> RESUMED <====");
-    } else if (state == AppLifecycleState.detached) {
-      print("====> DETACHED <====");
-    }
-  }
+class _MainPageState extends State<MainPage> {
+  bool isAvailable = false;
+  bool isAuthenticated = false;
+  String text = "Please Check Biometric Availability";
+  LocalAuthentication localAuthentication = LocalAuthentication();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Application State Demo")),
-      body: Container(),
+      appBar: AppBar(title: Text("Biometric Authentication")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              width: 200,
+              margin: EdgeInsets.only(bottom: 6),
+              child: RaisedButton(
+                child: Text(
+                  "Check Biometrics",
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onPressed: () async {
+                  isAvailable = await localAuthentication.canCheckBiometrics;
+                  if (isAvailable) {
+                    List<BiometricType> types =
+                        await localAuthentication.getAvailableBiometrics();
+                    text = "Biometrics Available:";
+                    for (var item in types) {
+                      text += "\n- $item";
+                    }
+                  }
+                  setState(() {});
+                },
+              ),
+            ),
+            Container(
+              width: 200,
+              child: RaisedButton(
+                child: Text(
+                  "Authenticate",
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onPressed: (isAvailable)
+                    ? () async {
+                        isAuthenticated = await localAuthentication
+                            .authenticateWithBiometrics(
+                                localizedReason: "Please Authenticate",
+                                stickyAuth: true,
+                                useErrorDialogs: true);
+                        setState(() {});
+                      }
+                    : null,
+              ),
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              margin: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: (isAuthenticated) ? Colors.green : Colors.red,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 3,
+                    spreadRadius: 2,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 2,
+              color: Colors.grey[200],
+              child: Center(
+                child: Text(
+                  text,
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
