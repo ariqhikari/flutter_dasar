e1790aa1d7613f3d4e440ddeaa6cda806e30b297
Ariq Hikari <ahidayatbia@gmail.com>
2020-12-06 17:17:02 +0700

    FLUTTER 95. Guided Camera (Kamera dengan Panduan)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:my_first_flutter/ui/main_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,83 +9,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("URL Launcher Demo")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            LaunchButton("Call", () async {
-              await call("+6285156567283");
-            }),
-            LaunchButton("SMS", () async {
-              await sendSMS("+6285156567283");
-            }),
-            LaunchButton("Email", () async {
-              await sendEmail("ahidayatbia@gmail.com");
-            }),
-            LaunchButton("URL", () async {
-              await openURL(
-                "https://google.com",
-                forceWebView: true,
-                enableJavascript: true,
-              );
-            }),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class LaunchButton extends StatelessWidget {
-  final String text;
-  final Function onTap;
-
-  LaunchButton(this.text, this.onTap);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 200,
-      child: RaisedButton(
-        child: Text(text),
-        color: Colors.blue[100],
-        onPressed: onTap,
-      ),
-    );
-  }
-}
-
-Future<void> call(String phoneNumber) async {
-  await launch("tel:$phoneNumber");
-}
-
-Future<void> sendSMS(String phoneNumber) async {
-  await launch("sms:$phoneNumber");
-}
-
-Future<void> sendEmail(String email) async {
-  await launch("mailto:$email");
-}
-
-Future<void> openURL(
-  String url, {
-  bool forceWebView = false,
-  bool enableJavascript = false,
-}) async {
-  if (await canLaunch(url)) {
-    await launch(
-      url,
-      forceWebView: forceWebView,
-      enableJavaScript: enableJavascript,
     );
   }
 }
